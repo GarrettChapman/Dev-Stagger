f6eadbbe9c40253da2109fd68403d2e3ab2d0167
Garrett <garrett.chapman@yahoo.com>
2018-02-08 19:06:02 -0600

    updates

--- public/js/controllers/infoCtrl.js
+++ public/js/controllers/infoCtrl.js
@@ -1,3 +1,9 @@
 angular.module('myApp').controller('infoCtrl', function($scope){
-  $scope.test = "working>?"
+  $scope.test = "working>?";
+
+//   $scope.getRandomBar = function() {
+//     var random = Math.floor(Math.random() * bars.length)
+//     var randomBar = bars.slice(random, random + 1)
+//     map(randomBar)
+// }
 })
